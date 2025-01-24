8762f5346bf4a2931b9aab2b1ba45ceb9bc1066c
gabriela712 <mariagabrielatgm@gmail.com>
2025-01-25 00:23:39 +0100

    Exercise

---
+++ src/MetodosDeCadenaComunes_String/Exercise.java
@@ -0,0 +1,26 @@
+package MetodosDeCadenaComunes_String;
+
+import javax.management.MBeanAttributeInfo;
+
+public class Exercise {
+    public static void main(String[] args) {
+        // Declarar e inicializar las variables
+        String firstName = "Harry";
+        String lastName = "Grow";
+        String email = "harry.grow@example.com";
+
+        // Convertir a minúsculas y concatenar firstName y lastName
+        String username = firstName.toLowerCase() + lastName.toLowerCase();
+        System.out.println("Username: " + username);
+
+        // Corregir la dirección de correo electrónico
+        email = email.replace("example.com", "gmail.com");
+        System.out.println("Corrected email: " + email);
+
+        // Encontrar el índice de '@'
+        int atIndex = email.indexOf('@');
+        System.out.println("Index of '@': " + atIndex);
+
+
+    }
+}
