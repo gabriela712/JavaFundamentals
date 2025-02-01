9fef5e94e9944cf69ffdd1ec90a0a0dd5417d389
gabriela712 <mariagabrielatgm@gmail.com>
2025-02-01 01:22:19 +0100

    String with index numbers

---
+++ src/manipulatingStrings/ManipulateaStringWithIndexNumbers.java
@@ -0,0 +1,16 @@
+package manipulatingStrings;
+
+public class ManipulateaStringWithIndexNumbers {
+    public static void main(String[] args) {
+        //indexOf(String stringToSearchFor)
+        String testString = "This is a test which is nice";
+        int indexOfSearch1= testString.indexOf("Java"); // returns -1
+        int indexOfSearch2 = testString.indexOf("is"); // returns 2
+        int indexOfSearch3 = testString.indexOf("est"); // returns 11
+
+        System.out.println("testString.indexOf(\"Java\") : " + testString.indexOf("Java"));
+        System.out.println("testString.indexOf(\"is\") : " + testString.indexOf("is"));
+        System.out.println("testString.indexOf(\"est\") : " + testString.indexOf("est"));
+
+    }
+}
